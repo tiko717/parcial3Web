df70b6ed8598bd8c01d61e18d09f32c69bc62e7b
Francisco José Del Río Domínguez <0610915314@uma.es>
2024-12-13 15:27:40 +0100

    Update APIContext.jsx

--- client/src/context/APIContext.jsx
+++ client/src/context/APIContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const APIContext = createContext();
 
 //const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL || "http://localhost:8000/api";
-const BASE_URL = "https://parcial3-web-server.vercel.app";
+const BASE_URL = "https://parcial3-web-server.vercel./api";
 
 export const APIProvider = ({ children }) => {
     const [loadCount, setLoadCount] = useState(0);
